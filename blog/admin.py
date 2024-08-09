d355be1f0f7598ad9f9c7f42f4f2913d46fa5e22
Mohamad <abdelrahim153@gmail.com>
2024-08-09 08:21:14 +0200

    First commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from blog.models import BlogPost
+
+
+admin.site.register(BlogPost)
+
+SECRET_KEY = 'django-insecure-xqplk$(1982row@k(t2)o=_hj_a0*@41!-r9og8$x__ag*4r(_'
